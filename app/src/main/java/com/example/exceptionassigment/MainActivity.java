ce8788d9b90b8e7d02ec8b88bd32828504fdabd2
Zur <yarden7671@gmail.com>
2024-02-07 11:35:54 +0200

    commit all

--- app/src/main/java/com/example/exceptionassigment/MainActivity.java
+++ app/src/main/java/com/example/exceptionassigment/MainActivity.java
@@ -11,15 +11,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int [] arr = new int [10];
-        try {
-            int x= arr[30]; //חריגה מגבולות המערך
-        }
-        catch (ArrayIndexOutOfBoundsException e)
-        {
-            System.out.println("An error occurred while accessing the array: " + e.getMessage());
-        }
-
+            //חריגה 1
+            int[] arr = new int[10];
+            try {
+                int x = arr[30]; //חריגה מגבולות המערך
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("An error occurred while accessing the array: " + e.getMessage());
+            }
+            //חריגה 2
         try {
             String str = null;
             int length = str.length();
@@ -28,9 +27,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+            try {
+                firstFunction();
+            } catch (Exception e) {
+                System.out.println("Exception caught in main: " + e.getMessage());
+            }
+        }
 
-
-
-
+        //פונקציה ראשונה
+        public static void firstFunction() {
+            try {
+                secondFunction(); //קריאה לפונקציה השנייה
+            } catch (Exception e) {
+                System.out.println("Exception caught in firstFunction: " + e.getMessage());
+            }
+        }
+        //פונקציה שנייה
+        public static void secondFunction() throws Exception {
+            thirdFunction(); //קריאה לפונקציה השלישית
+        }
+        //פונקציה שלישית
+        public static void thirdFunction() throws Exception {
+            // יצירת חריגה
+            throw new Exception("This is an exception from thirdFunction.");
+        }
     }
-}
